94dc731cd63089ab18a571a8dc48bdc0bf7a83c1
federico29 <federico.barrios@mail.escuelaing.edu.co>
2021-01-27 09:47:05 -0500

    Parte 1

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,30 @@
 package edu.eci.arsw.primefinder;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
+public class Main {
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		Long inicio = System.currentTimeMillis()/1000;
+		int n=3;
+		LinkedList<PrimeFinderThread> threads = new LinkedList<>();
+
+		int average = 30000000/n;
+		int cont = 0;
+
+		for (int i = 0; i < n; i++){
+			threads.add(new PrimeFinderThread(cont,cont+average));
+			cont = cont + average;
+		}
+
+		for (PrimeFinderThread hilo: threads){
+			hilo.start();
+			hilo.suspenderhilo();
+			System.out.println("Hilo suspendido");
+			hilo.renaudarhilo();
+			System.out.println("Hilo reanudado");
+		}
 	}
-	
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -5,27 +5,40 @@ import java.util.List;
 
 public class PrimeFinderThread extends Thread{
 
-	
 	int a,b;
-	
 	private List<Integer> primes=new LinkedList<Integer>();
+	boolean suspender;
 	
 	public PrimeFinderThread(int a, int b) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.suspender = false;
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
+	public void run() {
+		for (int i = a; i <= b; i++) {
+			if (isPrime(i)) {
 				primes.add(i);
 				System.out.println(i);
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			synchronized (this) {
+				while (suspender) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
-		
-		
 	}
+
 	
 	boolean isPrime(int n) {
 	    if (n%2==0) return false;
@@ -39,8 +52,14 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
+
+	synchronized void suspenderhilo(){
+		suspender=true;
+	}
+
+	synchronized void renaudarhilo(){
+		suspender=false;
+		notify();
+	}
 	
 }
